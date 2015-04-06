744390bd7f878309f69154dc365f177279608315
majkl <m.madr@seznam.cz>
2015-04-06 10:21:09 +0200

    Test temporarily made incorrect

--- program_1/main.cpp
+++ program_1/main.cpp
@@ -18,8 +18,8 @@ int main(int argc, char** argv)
 {
    try
    {
-      if (myMin(2,5) != 2)
-         throw runtime_error("Test failure: myMin(2, 5) != 2");
+      if (myMin(2,5) != 3)
+         throw runtime_error("Test failure: myMin(2, 5) != 3");
 
       if (myMin(9,5) != 5)
          throw runtime_error("Test failure: myMin(9, 5) != 5");
